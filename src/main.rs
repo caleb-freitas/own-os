e7c1767ef7d404c9b89a393bb20c5c9b42ba3541
caleb-freitas <calebfreitas@tutanota.com>
2023-01-03 16:52:40 -0300

    feat: implement `_start` function

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Goodbye, world!";
+
 // overwrites the os entry point to our own `_start` impl
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
